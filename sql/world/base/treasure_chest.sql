55e2204e9a111d60e6e3848c619280e97fa82ad5
BeardBear <138500494+BeardBear33@users.noreply.github.com>
2025-10-01 08:18:35 +0200

    Update treasure_chest.sql
    treasure respawn fix

--- sql/world/base/treasure_chest.sql
+++ sql/world/base/treasure_chest.sql
@@ -3,16 +3,16 @@ DELETE FROM `gameobject_template` WHERE `entry` IN (990200,990201,990202,990210,
 
 -- CS
 INSERT INTO `gameobject_template`
-(`entry`,`type`,`displayId`,`name`,`size`,`Data0`,`Data1`,`Data3`,`Data10`,`Data12`,`Data15`)
+(`entry`,`type`,`displayId`,`name`,`size`,`Data0`,`Data1`,`Data3`,`Data10`,`Data12`,`Data15`,`ScriptName`)
 VALUES
-(990200,3,259,'Prostá truhla s pokladem',1,43,990200,1,1,1,1),
-(990201,3,259,'Vzácná truhla s pokladem',1,43,990201,1,1,1,1),
-(990202,3,259,'Epická truhla s pokladem',1,43,990202,1,1,1,1);
+(990200,3,259,'Prostá truhla s pokladem',1,43,990200,1,1,1,1,'TreasureChestPersistGO'),
+(990201,3,259,'Vzácná truhla s pokladem',1,43,990201,1,1,1,1,'TreasureChestPersistGO'),
+(990202,3,259,'Epická truhla s pokladem',1,43,990202,1,1,1,1,'TreasureChestPersistGO');
 
 -- EN
 INSERT INTO `gameobject_template`
-(`entry`,`type`,`displayId`,`name`,`size`,`Data0`,`Data1`,`Data3`,`Data10`,`Data12`,`Data15`)
+(`entry`,`type`,`displayId`,`name`,`size`,`Data0`,`Data1`,`Data3`,`Data10`,`Data12`,`Data15`,`ScriptName`)
 VALUES
-(990210,3,259,'Plain Treasure Chest',1,43,990200,1,1,1,1),
-(990211,3,259,'Rare Treasure Chest',1,43,990201,1,1,1,1),
-(990212,3,259,'Epic Treasure Chest',1,43,990202,1,1,1,1);
+(990210,3,259,'Plain Treasure Chest',1,43,990200,1,1,1,1,'TreasureChestPersistGO'),
+(990211,3,259,'Rare Treasure Chest',1,43,990201,1,1,1,1,'TreasureChestPersistGO'),
+(990212,3,259,'Epic Treasure Chest',1,43,990202,1,1,1,1,'TreasureChestPersistGO');
